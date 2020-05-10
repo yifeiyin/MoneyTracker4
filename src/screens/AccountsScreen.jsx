80946f1dcdb653408ca4d1d0bb19c59be636cf91
Yifei Yin <yifeiyin@foxmail.com>
2020-05-10 19:01:15 -0400

    Some clean up and functionality development

--- src/screens/AccountsScreen.jsx
+++ src/screens/AccountsScreen.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button, Typography } from '@material-ui/core';
+import { ArrowDropDown as ArrowDropDownIcon, ArrowDropUp as ArrowDropUpIcon } from '@material-ui/icons';
 
 import ObjectEditor from '../ObjectEditor/index';
 import { AccountTreeView, Modal, TransactionList } from '../components';
@@ -14,6 +15,7 @@ class AccountsScreen extends React.Component {
     currentAccountId: null,
 
     selectedAccountName: '',
+    selectedTransactionsSummary: {},
   }
 
   componentDidMount() {
@@ -81,42 +83,15 @@ class AccountsScreen extends React.Component {
     } else {
       name = global.accountManager.fromIdToName(id);
     }
-    this.setState({ selectedAccountName: name });
 
-    const allTransactionsObj = global.transactionContainer.getAllTransactions();
-    const allTransactions = Object.keys(allTransactionsObj).map(k => ({ id: k, ...allTransactionsObj[k] }));
-    const accountNodeHierarchy = global.accountManager._accountNodeHierarchy;
-    const endAccountsToInclude = [];
-    addChild(id, endAccountsToInclude);
+    const targetAccountType = global.accountManager._accountNodes[id].accountType;
+    console.assert(['debit', 'credit'].includes(targetAccountType), 'Unexpected value: ' + targetAccountType);
+    const [transactionsForThisAccount, accountsIncluded] = getTransactionsForAccount(id);
+    this.TransactionView.setData(transactionsForThisAccount);
+    const selectedTransactionsSummary = getTransactionsSummary(transactionsForThisAccount, accountsIncluded, targetAccountType);
 
-    const toShow = allTransactions.filter(transaction => {
-      const { debits, credits } = transaction;
-      for (let [acc, monum] of debits) {
-        if (endAccountsToInclude.includes(String(acc))) return true;
-        console.assert(typeof (acc) === typeof (endAccountsToInclude[0]), 'Type is different', typeof (acc), typeof (endAccountsToInclude[0]), transaction.id);
-      }
-      for (let [acc, monum] of credits) {
-        if (endAccountsToInclude.includes(String(acc))) return true;
-        console.assert(typeof (acc) === typeof (endAccountsToInclude[0]), 'Type is different', typeof (acc), typeof (endAccountsToInclude[0]), transaction.id);
-      }
-      return false;
-    });
+    this.setState({ selectedAccountName: name, selectedTransactionsSummary });
 
-    this.TransactionView.setData(toShow);
-
-    function isEndNode(id) {
-      return !global.accountManager._accountNodes[id].isFolder;
-    }
-
-    function addChild(currentTarget, result) {
-      if (isEndNode(currentTarget)) {
-        result.push(currentTarget);
-        return;
-      }
-      for (let child of accountNodeHierarchy[currentTarget].children) {
-        addChild(child, result);
-      }
-    }
   }
 
   render() {
@@ -145,6 +120,24 @@ class AccountsScreen extends React.Component {
 
         <div style={{ flex: 3 }}>
           <Typography variant='h4' align='center'>{this.state.selectedAccountName}</Typography>
+
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <div>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <ArrowDropUpIcon style={{ color: 'green' }} />
+                {String(this.state.selectedTransactionsSummary.negativeChange) || '--'}
+              </div>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <ArrowDropDownIcon style={{ color: 'red' }} />
+                {String(this.state.selectedTransactionsSummary.positiveChange) || '--'}
+              </div>
+            </div>
+            <div style={{ width: 1, backgroundColor: 'black', margin: '5px 20px', alignSelf: 'stretch' }}></div>
+            <div style={{ fontSize: 26 }}>
+              {String(this.state.selectedTransactionsSummary.final)}
+            </div>
+          </div>
+
           <TransactionList
             ref={(o) => this.TransactionView = o}
             viewOnly={true}
@@ -157,5 +150,90 @@ class AccountsScreen extends React.Component {
     );
   }
 }
+
+function getTransactionsForAccount(id) {
+
+  const allTransactionsObj = global.transactionContainer.getAllTransactions();
+  const allTransactions = Object.keys(allTransactionsObj).map(k => ({ id: k, ...allTransactionsObj[k] }));
+  const accountNodeHierarchy = global.accountManager._accountNodeHierarchy;
+  const endAccountsToInclude = [];
+  addChild(id, endAccountsToInclude);
+
+  const toShow = allTransactions.filter(transaction => {
+    const { debits, credits } = transaction;
+    for (let [acc, monum] of debits) {
+      if (endAccountsToInclude.includes(String(acc))) return true;
+      console.assert(typeof (acc) === typeof (endAccountsToInclude[0]), 'Type is different', typeof (acc), typeof (endAccountsToInclude[0]), transaction.id);
+    }
+    for (let [acc, monum] of credits) {
+      if (endAccountsToInclude.includes(String(acc))) return true;
+      console.assert(typeof (acc) === typeof (endAccountsToInclude[0]), 'Type is different', typeof (acc), typeof (endAccountsToInclude[0]), transaction.id);
+    }
+    return false;
+  });
+
+  return [toShow, endAccountsToInclude];
+
+  function isEndNode(id) {
+    return !global.accountManager._accountNodes[id].isFolder;
+  }
+
+  function addChild(currentTarget, result) {
+    if (isEndNode(currentTarget)) {
+      result.push(currentTarget);
+      return;
+    }
+    for (let child of accountNodeHierarchy[currentTarget].children) {
+      addChild(child, result);
+    }
+  }
+
+}
+
+
+function getTransactionsSummary(transactions, accounts, accountType) {
+  const Monum = global.Monum;
+  const initial = new Monum();
+
+  let debitChange = new Monum(), creditChange = new Monum();
+  for (let transaction of transactions) {
+    const { debits, credits } = transaction;
+    for (let [targetAcc, monum] of debits) {
+      if (accounts.includes(targetAcc))
+        debitChange = Monum.combine(debitChange, monum);
+    }
+
+    for (let [targetAcc, monum] of credits) {
+      if (accounts.includes(targetAcc))
+        creditChange = Monum.combine(creditChange, monum);
+    }
+  }
+
+  let final, netChange, positiveChange, negativeChange;
+  if (accountType === 'debit') {
+    positiveChange = debitChange;
+    negativeChange = creditChange;
+
+  } else if (accountType === 'credit') {
+    positiveChange = creditChange;
+    negativeChange = debitChange;
+
+  } else {
+    throw new Error('Unexpected value accountType: ' + accountType);
+  }
+
+  netChange = positiveChange.sub(negativeChange);
+  final = initial.add(netChange);
+
+  return {
+    count: transactions.length,
+    initial,
+    final,
+    netChange,
+    positiveChange,
+    negativeChange,
+  };
+}
+
 
 export default withSnackbar(AccountsScreen);
